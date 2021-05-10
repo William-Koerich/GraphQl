59d0f4ccdf469dadc321bbc9fb37f761d7859c42
William Koerich <williamkoerich@hotmail.com>
2021-05-10 13:48:48 -0300

    fix: fixed orthography

--- src/startServer.js
+++ src/startServer.js
@@ -3,7 +3,7 @@ import { ApolloServer } from 'apollo-server'
 function startServer({ typeDefs, resolvers }) {
   const server = new ApolloServer({ typeDefs, resolvers })
 
-  server.listen(7777).then(({ url }) => console.log(`🔥Server started ar ${url}`))
+  server.listen(7777).then(({ url }) => console.log(`🔥Server started at ${url}`))
 }
 
 export default startServer;
